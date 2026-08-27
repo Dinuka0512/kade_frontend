430615ff007ea7f72a236a5809e724ce3ba41e24
Dinuka Lakmal <177092200+Dinuka0512@users.noreply.github.com>
2026-08-28 03:07:37 +0530

    env Updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,16 @@
 const nextConfig = {
-  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
